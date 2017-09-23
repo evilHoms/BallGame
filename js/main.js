9f31274cc68351edaad7f7a51b05188858ee3ff5
homs <homspage@gmail.com>
2017-09-23 18:16:49 +0300

    Creation of active balls added and event listeners

--- js/main.js
+++ js/main.js
@@ -15,22 +15,21 @@ const activeBalls = [];
 
 const BALLS_RADIUS = 20;
 const TARGET_BALLS_COLOR = `#add`;
-const NUMBER_OF_BALLS = { easy: 3, normal: 6, hard: 10, nightmare: 20 };
+const NUMBER_OF_BALLS = { easy: { target: 3, active: 5 }, 
+                         normal: { target: 6, active: 4 }, 
+                         hard: { target: 10, active: 3 }, 
+                         nightmare: { target: 20, active: 1 } 
+                        };
 
 
+window.addEventListener(`resize`, winResizeEvent);
+window.addEventListener(`mousemove`, mouseMoveEvent);
+window.addEventListener(`mousedown`, activeBallMouseDown);
+window.addEventListener(`mouseup`, activeBallMouseUp);
 
-window.addEventListener(`resize`, () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-});
+addActiveBalls(c, NUMBER_OF_BALLS.nightmare.active, ground);
+addTargetBalls(c, NUMBER_OF_BALLS.nightmare.target, ground);
 
-window.addEventListener(`mousemove`, (e) => {
-  mouse.x = e.x;
-  mouse.y = e.y;
-});
-
-
-addBallsToArr(c, targetBalls, NUMBER_OF_BALLS.nightmare);
 animate();
 
 
@@ -40,6 +39,7 @@ function animate() {
   clear();
   
   ground.update();
+  updateObjectsArray(c, activeBalls);
   updateObjectsArray(c, targetBalls);
   cursor.update(mouse.x, mouse.y);
 }
@@ -58,59 +58,85 @@ function updateObjectsArray(ctx, objArray) {
   });
 }
 
-/* Создаем объекты Ball и помещаем их в переданный массив, 
+/* Создаем объекты Ball и помещаем их в массив предназначенный
+  для объектов, в которые нужно попасть, 
   отрисовки в этом методе не происходит
 */
-function addBallsToArr(ctx, objArray, numberOfElements, area) {
+function addTargetBalls(ctx, numberOfElements, unavailableArea) {
   
   let availableXY;
   
   for (let i = 0; i < numberOfElements; i++) {
     
-    availableXY = randomBallPosition(objArray, BALLS_RADIUS, ground);
-    objArray.push(new Ball(ctx, availableXY.x, availableXY.y, BALLS_RADIUS, TARGET_BALLS_COLOR));
+    availableXY = randomBallPosition(targetBalls, BALLS_RADIUS, unavailableArea);
+    
+    targetBalls.push(new Ball(ctx, availableXY.x, availableXY.y, BALLS_RADIUS, TARGET_BALLS_COLOR));
     
   }
   
-  availableXY = randomBallPosition(objArray, BALLS_RADIUS, ground);
-  objArray.push(new Ball(ctx, availableXY.x, availableXY.y, BALLS_RADIUS, `#f00`));
+  
+  /* Создаем рандомные координаты, новый обхект с которыми не будет соприкосаться
+    с остальными объектами своего массива и будет находиться в определенной
+    области
+  */
+  function randomBallPosition(objArray, radius, unavailableArea) {
 
-}
+    let isCoordsAvailable = true;
+    let x = 0;
+    let y = 0;
 
-/* Создаем рандомные координаты, новый обхект с которыми не будет соприкосаться
-  с остальными объектами своего массива и будет находиться в определенной
-  области
-*/
-function randomBallPosition(objArray, radius, wrongArea) {
+    do {
 
-  let isCoordsAvailable = true;
-  let x = 0;
-  let y = 0;
+      isCoordsAvailable = true;
 
-  do {
+      x = Math.round(Math.random() * (innerWidth - radius * 3)) + radius * 1.5;
 
-    isCoordsAvailable = true;
+      y = Math.round(Math.random() * (innerHeight - radius * 3)) + radius * 1.5;
 
-    x = Math.round(Math.random() * (innerWidth - radius * 3)) + radius * 1.5;
+      /* Проверяем, не будет ли объект с данными координатами
+      накладываться на уже созданные объекты
+      */
+      objArray.forEach((el) => {
+        if (Math.sqrt(Math.pow(x - el.x, 2) + Math.pow(y - el.y, 2)) < radius * 2) {
+          isCoordsAvailable = false;
+        }
+      });
 
-    y = Math.round(Math.random() * (innerHeight - radius * 3)) + radius * 1.5;
-
-    /* Проверяем, не будет ли объект с данными координатами
-    накладываться на уже созданные объекты
-    */
-    objArray.forEach((el) => {
-      if (Math.sqrt(Math.pow(x - el.x, 2) + Math.pow(y - el.y, 2)) < radius * 2) {
+      /* Проерить, не будут ли координаты соприкасаться с определенными областями
+      */
+      if (Math.sqrt(Math.pow(x - unavailableArea.x, 2) + Math.pow(y - unavailableArea.y, 2)) < radius + unavailableArea.r) {
         isCoordsAvailable = false;
       }
-    });
 
-    /* Проерить, не будут ли координаты соприкасаться с определенными областями
-    */
-    if (Math.sqrt(Math.pow(x - wrongArea.x, 2) + Math.pow(y - wrongArea.y, 2)) < radius + wrongArea.r) {
-      isCoordsAvailable = false;
-    }
+    } while (!isCoordsAvailable);
 
-  } while (!isCoordsAvailable);
+    return { x, y };
+  }
+  
+}
 
-  return { x, y };
+function addActiveBalls(ctx, numberOfElements, startingArea) {
+  for (let i = 0; i < numberOfElements; i++) {
+     activeBalls.push(new Ball(ctx, startingArea.x, 
+                               startingArea.y - startingArea.r + BALLS_RADIUS * 1.5, 
+                               BALLS_RADIUS, `#f00`));
+  }
+}
+
+function winResizeEvent(e) {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+}
+
+function mouseMoveEvent(e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+}
+
+function activeBallMouseDown(e) {
+  console.log(`mouseDown`);
+}
+
+function activeBallMouseUp(e) {
+  console.log(`mouseUp`);
 }
